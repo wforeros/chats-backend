ec8de9e51b50eede0bdcedfadcd24824e04eb19a
Wilson Forero <wilson_camilo@hotmail.com>
2021-03-03 01:05:00 -0500

    First commit

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,6 @@ import Config from "./config/config";
 import db from "mongoose";
 db.Promise = global.Promise;
 
-//mongodb+srv://db_user_wilsonf:WilsonCamilo01@cluster0.ihfqa.mongodb.net/telegrom
 export default async function connectToDatabase() {
 	console.log("Conectando db");
 	const URL = Config.DB_URL;
